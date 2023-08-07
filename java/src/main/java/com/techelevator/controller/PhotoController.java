4b145020c1141871b692fab6278f145c3ba3eb95
Aarti Kansal <aartilumb@gmail.com>
2023-08-07 12:01:49 -0400

    pics.txt added with images link

--- java/src/main/java/com/techelevator/controller/PhotoController.java
+++ java/src/main/java/com/techelevator/controller/PhotoController.java
@@ -1,10 +1,20 @@
 package com.techelevator.controller;
 
 
+import com.techelevator.dao.PhotoDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api/photos")
 public class PhotoController {
+    @Autowired
+    private PhotoDao photoDao;
+    //private PhotoService photoService;
+     public PhotoController(PhotoDao photoDao){
+         this.photoDao = photoDao;
+     }
 }
