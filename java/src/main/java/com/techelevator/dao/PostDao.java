adfdca97b0c0387ed78cb40f9d0fc83b215163fb
Brian Rosander <kitchen2code@gmail.com>
2023-08-07 16:49:10 -0400

    DAO resets

---
+++ java/src/main/java/com/techelevator/dao/PostDao.java
@@ -0,0 +1,16 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Post;
+
+import java.util.List;
+
+public interface PostDao {
+<<<<<<< HEAD
+=======
+    List<Post> getPosts();
+
+
+>>>>>>> e11db964b3f5a0f28eb7540fefa14782ce1bf90a
+
+    List<Post> getAllPosts();
+}
