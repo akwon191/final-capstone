97db5014504bf4b91e593c0cd8c3aa0aec4ded03
Brian Rosander <kitchen2code@gmail.com>
2023-08-08 15:43:25 -0400

    GetAllPosts Post DAO COMPLETE and TESTED -authorization enabled

--- java/src/main/java/com/techelevator/controller/PostController.java
+++ java/src/main/java/com/techelevator/controller/PostController.java
@@ -7,7 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-//@PreAuthorize("isAuthenticated()")
+@PreAuthorize("isAuthenticated()")
 @RequestMapping("/posts")
 @RestController
 @CrossOrigin
