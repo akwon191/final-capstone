b4c89483ca72ba09628a0d7a76620539d97f5679
Andrew Kwon <akwon191@gmail.com>
2023-08-08 16:51:45 -0400

    Edited data file

--- java/database/data.sql
+++ java/database/data.sql
@@ -15,6 +15,10 @@ VALUES ('brianr', 'techelevator1', 'ROLE_USER'),
 ('brodinm', 'techelevator1', 'ROLE_USER'),
 ('andyk', 'techelevator1', 'ROLE_USER');
 
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+
 -- Posts
 INSERT INTO posts (user_id, date_time, caption, image_data_id)
 VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), '2023-01-01 18:03:20', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1),
@@ -24,6 +28,10 @@ VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), '2023-01-01 18:03
 ((SELECT user_id FROM users WHERE username = 'brodinm'), '2023-06-19 17:03:01', 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus.', 5),
 ((SELECT user_id FROM users WHERE username = 'andyk'), '2023-01-10 13:15:02', 'Duis convallis convallis tellus id interdum. Neque volutpat ac tincidunt vitae semper quis lectus nulla.', 6);
 
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+
 -- Post_Comments
 INSERT INTO post_comments (post_id, user_id, date_time, comment_text)
 VALUES ((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'brianr'), (SELECT user_id FROM users WHERE username = 'aartik'),'2023-01-01 23:59:59', 'Nice!'),
@@ -32,6 +40,10 @@ VALUES ((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id W
 ((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'beckyz'), (SELECT user_id FROM users WHERE username = 'brodinm'),'2023-12-31 23:59:59', 'Beautiful!'),
 ((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'brodinm'), (SELECT user_id FROM users WHERE username = 'andyk'),'2023-12-31 23:59:59', 'Cool!'),
 ((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'andyk'), (SELECT user_id FROM users WHERE username = 'brianr'),'2023-12-31 23:59:59', 'How fun!');
+
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
 
 --User_Following
 INSERT INTO user_following (user_id, follower_id)
@@ -48,6 +60,10 @@ VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), (SELECT user_id F
 ((SELECT user_id FROM users WHERE username = 'andyk'), (SELECT user_id FROM users WHERE username = 'brianr')),
 ((SELECT user_id FROM users WHERE username = 'andyk'), (SELECT user_id FROM users WHERE username = 'aartik'));
 
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+
 -- Vibes
 INSERT INTO vibes (user_id, post_id)
 VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'aartik')),
@@ -56,6 +72,10 @@ VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), (SELECT post_id F
 ((SELECT user_id FROM users WHERE username = 'beckyz'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'brodinm')),
 ((SELECT user_id FROM users WHERE username = 'brodinm'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'andyk')),
 ((SELECT user_id FROM users WHERE username = 'andyk'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'brianr'));
+
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
 
 --Thanks
 INSERT INTO thanks (user_id, post_id)
@@ -66,6 +86,10 @@ VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), (SELECT post_id F
 ((SELECT user_id FROM users WHERE username = 'brodinm'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'andyk')),
 ((SELECT user_id FROM users WHERE username = 'andyk'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'brianr'));
 
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+
 --No_Thanks
 INSERT INTO no_thanks (user_id, post_id)
 VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'yoavm')),
@@ -74,9 +98,6 @@ VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), (SELECT post_id F
 ((SELECT user_id FROM users WHERE username = 'beckyz'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'andyk')),
 ((SELECT user_id FROM users WHERE username = 'brodinm'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'brianr')),
 ((SELECT user_id FROM users WHERE username = 'andyk'), (SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'aartik'));
-
---Profiles
-INSERT INTO profiles
 
 COMMIT TRANSACTION;
 
