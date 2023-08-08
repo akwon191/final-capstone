a4371b1220b7aa0d3c868fee8ae98037f377e898
Andrew Kwon <akwon191@gmail.com>
2023-08-08 14:34:20 -0400

    Edited schema and updated mock users, posts, and post_comment tables

--- java/database/data.sql
+++ java/database/data.sql
@@ -16,11 +16,25 @@ VALUES ('brianr', 'techelevator1', 'ROLE_USER'),
 ('andyk', 'techelevator1', 'ROLE_USER');
 
 -- Posts
-INSERT INTO posts (user_id, date_time, caption)
-VALUES (3, '2023-01-01 18:03:20', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
-(4, '2023-05-11 10:06:10', 'Vulputate dignissim suspendisse in est ante in nibh mauris. Maecenas sed enim ut sem viverra aliquet.'),
-(5, '2023-03-21', 'Risus feugiat in ante metus dictum at tempor commodo ullamcorper.'),
-(6, '2023-02-15 05:15:19', 'At risus viverra adipiscing at in tellus.'),
-(7, '2023-06-19 17:03:01', 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus.'),
-(8, '2023-01-10 13:15:02', 'Duis convallis convallis tellus id interdum. Neque volutpat ac tincidunt vitae semper quis lectus nulla.');
+INSERT INTO posts (user_id, date_time, caption, image_data_id)
+VALUES ((SELECT user_id FROM users WHERE username = 'brianr'), '2023-01-01 18:03:20', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1),
+((SELECT user_id FROM users WHERE username = 'aartik'), '2023-05-11 10:06:10', 'Vulputate dignissim suspendisse in est ante in nibh mauris. Maecenas sed enim ut sem viverra aliquet.', 2),
+((SELECT user_id FROM users WHERE username = 'yoavm'), '2023-03-21', 'Risus feugiat in ante metus dictum at tempor commodo ullamcorper.', 3),
+((SELECT user_id FROM users WHERE username = 'beckyz'), '2023-02-15 05:15:19', 'At risus viverra adipiscing at in tellus.', 4),
+((SELECT user_id FROM users WHERE username = 'brodinm'), '2023-06-19 17:03:01', 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus.', 5),
+((SELECT user_id FROM users WHERE username = 'andyk'), '2023-01-10 13:15:02', 'Duis convallis convallis tellus id interdum. Neque volutpat ac tincidunt vitae semper quis lectus nulla.', 6);
+
+-- Post_Comments
+INSERT INTO post_comments (post_id, user_id, date_time, comment_text)
+VALUES ((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'brianr'), (SELECT user_id FROM users WHERE username = 'aartik'),'2023-01-01 23:59:59', 'Nice!'),
+((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'aartik'), (SELECT user_id FROM users WHERE username = 'yoavm'),'2023-12-31 23:59:59', 'Great!'),
+((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'yoavm'), (SELECT user_id FROM users WHERE username = 'beckyz'),'2023-12-31 23:59:59', 'Awesome!'),
+((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'beckyz'), (SELECT user_id FROM users WHERE username = 'brodinm'),'2023-12-31 23:59:59', 'Beautiful!'),
+((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'brodinm'), (SELECT user_id FROM users WHERE username = 'andyk'),'2023-12-31 23:59:59', 'Cool!'),
+((SELECT post_id FROM posts JOIN users ON posts.user_id = users.user_id WHERE username = 'andyk'), (SELECT user_id FROM users WHERE username = 'brianr'),'2023-12-31 23:59:59', 'How fun!');
+
+--User_Following
+
+
+
 
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, posts, post_comments, images, user_following, thanks, no_thanks, vibes, profiles;
+DROP TABLE IF EXISTS users, posts, post_comments, images, image_data, user_following, thanks, no_thanks, vibes, profiles;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -21,10 +21,12 @@ CREATE TABLE posts (
 
 CREATE TABLE post_comments (
         comment_id serial NOT NULL,
+        user_id int NOT NULL,
         post_id int NOT NULL,
         date_time TIMESTAMP NOT NULL,
         comment_text varchar (150),
         CONSTRAINT PK_comment PRIMARY KEY (comment_id),
+        CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users (user_id),
         CONSTRAINT FK_post_id FOREIGN KEY (post_id) REFERENCES posts (post_id)
 );
 
@@ -88,7 +90,7 @@ START TRANSACTION;
 DROP TABLE images;
 
 ALTER TABLE posts
-ADD COLUMN image_id INT,
-ADD CONSTRAINT FK_image FOREIGN KEY (image_id) REFERENCES image_data (image_data_id);
+ADD COLUMN image_data_id INT,
+ADD CONSTRAINT FK_image_data_id FOREIGN KEY (image_data_id) REFERENCES image_data (image_data_id);
 
 COMMIT;
