719da001982193c556438bb30e586ba2fae31b22
Aarti Kansal <aartilumb@gmail.com>
2023-08-08 12:41:01 -0400

    UserService done

---
+++ java/src/main/java/com/techelevator/service/UserService.java
@@ -0,0 +1,22 @@
+package com.techelevator.service;
+
+
+import com.techelevator.dao.UserDao;
+import com.techelevator.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+}
