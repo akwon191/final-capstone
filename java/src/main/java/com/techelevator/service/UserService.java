0e5ae4bcd1c93239424f3a2392404c435d900bd5
Brian Rosander <kitchen2code@gmail.com>
2023-08-08 12:55:01 -0400

    UserService update for all methods

--- java/src/main/java/com/techelevator/service/UserService.java
+++ java/src/main/java/com/techelevator/service/UserService.java
@@ -1,7 +1,6 @@
 package com.techelevator.service;
 
-
-import com.techelevator.dao.UserDao;
+import com.techelevator.dao.JdbcUserDao;
 import com.techelevator.model.User;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +9,33 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private final UserDao userDao;
+    private final JdbcUserDao userDao;
 
-    public UserService(UserDao userDao) {
+    public UserService(JdbcUserDao userDao) {
         this.userDao = userDao;
     }
 
     public List<User> findAll() {
         return userDao.findAll();
+    }
+
+    public int findIdByUsername(String username) {
+        return userDao.findIdByUsername(username);
+    }
+
+    public User getUserById(int userId) {
+        return userDao.getUserById(userId);
+    }
+
+    public List<User> findAllUsers() {
+        return userDao.findAll();
+    }
+
+    public User findByUsername(String username) {
+        return userDao.findByUsername(username);
+    }
+
+    public boolean createUser(String username, String password, String role) {
+        return userDao.create(username, password, role);
     }
 }
