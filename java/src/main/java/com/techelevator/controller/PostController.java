34443e3f4f5904021fa5e14a101446562459b0db
Aarti Kansal <aartilumb@gmail.com>
2023-08-08 13:14:01 -0400

    ImageDao done, @GetMapping for imageId done

--- java/src/main/java/com/techelevator/controller/PostController.java
+++ java/src/main/java/com/techelevator/controller/PostController.java
@@ -2,16 +2,19 @@ package com.techelevator.controller;
 
 
 import com.techelevator.dao.PostDao;
+import com.techelevator.dao.UserDao;
+import com.techelevator.model.Image;
 import com.techelevator.model.Post;
+import com.techelevator.model.User;
 import com.techelevator.service.ImageService;
 import com.techelevator.service.PostService;
+import com.techelevator.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import javax.validation.Valid;
 import java.util.List;
 
 @PreAuthorize("isAuthenticated()")
@@ -20,18 +23,35 @@ import java.util.List;
 @CrossOrigin
 
 public class PostController {
-    @Autowired
+        @Autowired
 
         private PostService postService;
         private ImageService imageService;
+        private UserService userService;
 
-        public PostController(PostService postService,ImageService imageService){
+    public PostController(PostService postService,ImageService imageService,UserService userService){
             this.imageService = imageService;
             this.postService = postService;
+            this.userService = userService;
         }
+    @GetMapping(path = "")
+    public List<User> listUsers() {
+        return userService.findAll();
+    }
 
     @GetMapping(path = "")
     public List<Post> getAllPosts() {
         return postService.getAllPosts();
     }
+
+    @GetMapping(path = "/{imageId}")
+    public Image getImageDataById(@Valid @PathVariable long imageId) {
+       // try {
+            return imageService.getImageDataStringById(imageId);
+      //  } catch (ResourceNotFoundException e) {
+      //      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+     //   }
+    }
+
+
 }
--- java/src/main/java/com/techelevator/dao/ImageDao.java
+++ java/src/main/java/com/techelevator/dao/ImageDao.java
@@ -1,4 +1,11 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Image;
+import com.techelevator.model.Post;
+
+import java.util.List;
+
 public interface ImageDao {
+
+    Image getImageDataStringById(long id);
 }
