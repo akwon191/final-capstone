7e7b6c468a142dbb9bc0213d61c5cd552ac9d816
Aarti Kansal <aartilumb@gmail.com>
2023-08-08 12:33:47 -0400

    controller---@getMapping added

--- java/src/main/java/com/techelevator/controller/PostController.java
+++ java/src/main/java/com/techelevator/controller/PostController.java
@@ -2,19 +2,36 @@ package com.techelevator.controller;
 
 
 import com.techelevator.dao.PostDao;
+import com.techelevator.model.Post;
+import com.techelevator.service.ImageService;
+import com.techelevator.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@PreAuthorize("isAuthenticated()")
+@RequestMapping("/posts")
 @RestController
 @CrossOrigin
-@RequestMapping("posts")
+
 public class PostController {
     @Autowired
-    private PostDao postDao;
-    //private Service postService;
-     public PostController(PostDao postDao){
-         this.postDao = postDao;
-     }
+
+        private PostService postService;
+        private ImageService imageService;
+
+        public PostController(PostService postService,ImageService imageService){
+            this.imageService = imageService;
+            this.postService = postService;
+        }
+
+    @GetMapping(path = "")
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
 }
