fde9573ea6174c9225094da6d4cfdcb2afe5a5e3
Andrew Kwon <akwon191@gmail.com>
2023-08-09 10:14:24 -0400

    Added PostService

---
+++ vue/src/services/PostService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:9000';
+
+export default {
+    listPosts() {
+        return axios.get(API_BASE_URL + '/posts')
+    },
+
+    getPost(postId) {
+        return axios.get(API_BASE_URL + '/posts/' + postId)
+    },
+    addPost(Post) {
+        return axios.post(API_BASE_URL + '/posts', Post)
+    },
+}
