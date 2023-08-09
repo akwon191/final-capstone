db6a8453b5f39f63730b87e1bbc496228b02c268
Aarti Kansal <aartilumb@gmail.com>
2023-08-09 12:00:52 -0400

    getUserById added for GetMapping

--- java/src/main/java/com/techelevator/controller/UserController.java
+++ java/src/main/java/com/techelevator/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.techelevator.controller;
 
+import com.techelevator.exception.ResourceNotFoundException;
 import com.techelevator.model.User;
 import com.techelevator.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 ;
+import javax.validation.Valid;
 import java.util.List;
 
 @PreAuthorize("isAuthenticated()")
@@ -25,6 +29,10 @@ public class UserController {
     @GetMapping(path = "")
     public List<User> listUsers() {
         return userService.findAll();
+    }
+    @GetMapping(path = "/{userId}")
+    public User getUserById(@Valid @PathVariable int userId) throws ResourceNotFoundException {
+        return userService.getUserById(userId);
     }
 
 }
