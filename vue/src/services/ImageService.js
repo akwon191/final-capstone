51e2c52fbf201f5ca5567f57538ee44ac7e29459
Aarti Kansal <aartilumb@gmail.com>
2023-08-09 10:20:22 -0400

    ImageService added

---
+++ vue/src/services/ImageService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:9000';
+
+export default {
+    listImage() {
+        return axios.get(API_BASE_URL +  "/{imageId}")
+    },
+
+    getImage(imageId) {
+        return axios.get(API_BASE_URL + '/images/' + imageId)
+    },
+    
+}
