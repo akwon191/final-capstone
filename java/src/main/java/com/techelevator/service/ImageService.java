bfd423a96becdab48b91e13445f03d0264ec5c5d
Aarti Kansal <aartilumb@gmail.com>
2023-08-10 12:13:49 -0400

    uploadImage method added to service

--- java/src/main/java/com/techelevator/service/ImageService.java
+++ java/src/main/java/com/techelevator/service/ImageService.java
@@ -18,4 +18,6 @@ public class ImageService {
     public Image getImageDataStringById(long id) {
         return imageDao.getImageDataStringById(id);
     }
+    public Image uploadImage(Image image) { return imageDao.uploadImage(image); }
+
 }
