c7f48f34e85c32b76ad4feafa661b6290c70a6fb
Brian Rosander <kitchen2code@gmail.com>
2023-08-13 19:19:58 -0400

    Header change

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
+      <page-headers />
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import PageHeaders from './components/Headers.vue'
+
+export default {
+  name: "home",
+  components: {
+    PageHeaders
+  }
+};
+</script>
--- vue/src/views/Profile.vue
+++ vue/src/views/Profile.vue
@@ -1,18 +1,15 @@
 <template>
     <div>
-      <page-headers />
       <profile-card />
     </div>
   </template>
   
   <script>
-  import PageHeaders from '../components/Headers.vue'
   import ProfileCard from '../components/ProfileCard.vue'
   
   export default {
     name: "profile",
     components: {
-      PageHeaders,
       ProfileCard
     }
   };
