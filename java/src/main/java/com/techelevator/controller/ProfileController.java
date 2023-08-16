057e903f21dc2ca90555684a4a0bc7e7fd3f3150
Aarti Kansal <aartilumb@gmail.com>
2023-08-16 14:31:51 -0400

    adjusted profileDao

--- java/src/main/java/com/techelevator/controller/ProfileController.java
+++ java/src/main/java/com/techelevator/controller/ProfileController.java
@@ -2,10 +2,15 @@ package com.techelevator.controller;
 
 
 
+import com.techelevator.model.Profile;
+import com.techelevator.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @PreAuthorize("isAuthenticated()")
 @RequestMapping("/profiles")
@@ -13,7 +18,36 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 
 public class ProfileController  {
+    private final ProfileService profileService;
 
+    @Autowired
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Integer> createProfile(@RequestBody Profile profile) {
+        int generatedProfileId = profileService.createProfile(profile);
+        return new ResponseEntity<>(generatedProfileId, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Profile> getProfileById(@PathVariable int userId) {
+        Profile profile = profileService.getProfileById(userId);
+        if (profile != null) {
+            return new ResponseEntity<>(profile, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{userId}/followers")
+    public ResponseEntity<List<Profile>> getFollowers(@PathVariable int userId) {
+        List<Profile> followers = profileService.getFollowers(userId);
+        return new ResponseEntity<>(followers, HttpStatus.OK);
+    }
 }
+
+
 
 
--- java/src/main/java/com/techelevator/dao/JdbcFollowerDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcFollowerDao.java
@@ -1,4 +1,8 @@
 package com.techelevator.dao;
 
-public class JdbcFollowerDao {
-}
+import com.techelevator.model.Profile;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.List;
+
